cce777501301ae4b733e65ca08c59c3d379e3c78
Christin Boner <christinmclemons@gmail.com>
2019-08-29 21:50:40 -0700

    working on skeleton javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,47 @@
+//create an array of words
+var words = ["poltergeist", "graveyard", "screams", "headless horseman", "creature", "monster", "goblin", "bogeyman", "grave", "intestines", "brimstone", "phantom", "worms", "clowns", "werewolf", "horror", "zombie", "doom"]
+//keep track of the wrong letters
+
+//keep track of lives
+
+//keep track of score
+
+//keep track of guesses left
+
+//function for onkeyup triggering functions for right or wrong choice
+
+//function for adjusting score 
+
+    //function for you win at score = 10
+
+//function for removing lives
+
+    //pseudo function for game over at 0 lives
+
+function wrongChoice(){
+    if
+    //show the onkeyup character in wrong answer section
+    if
+    //-1 in the guesses left section
+    else if
+    //IF guesses left is 0, -1 life
+        //if lives is 0, Game over
+    
+    else
+    //cycle to random new word
+    
+};
+//keep track of the right letters when they get pressed
+function  rightChoice(){
+    //if on keyup is a character in the hidden word, 
+        //reveal the letter
+        //do not affect guesses left
+
+    //IF a right choice completes a word
+        //+1 to the score
+        //choose new random word
+        //clear guessed letters
+
+    //IF score gets to 10
+    //YOU WIN
+} 
