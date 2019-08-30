31ea339cf88fded4913ab56c02b647d4446c5826
Christin Boner <christinmclemons@gmail.com>
2019-08-29 22:53:56 -0700

    commiting to submit as is

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,50 @@
 //create an array of words
-var words = ["poltergeist", "graveyard", "screams", "headless horseman", "creature", "monster", "goblin", "bogeyman", "grave", "intestines", "brimstone", "phantom", "worms", "clowns", "werewolf", "horror", "zombie", "doom"]
+var words = ["poltergeist", "graveyard", "screams", "headless horseman", "creature", "monster", "goblin", "bogeyman", "grave", "intestines", "brimstone", "phantom", "worms", "clowns", "werewolf", "horror", "zombie", "doom"];
+//create object for bse stats
+var stats = {
+    lives: 3,
+    guesses: 10,
+    score: 0,
+}
+//function for picking random words
+var word = words[Math.floor(Math.random() * words.length)];
+//this shows an underscore for each letter in the word chosen
+var answerArr = [];
+for (var i = 0; i < word.length; i++) {
+    answerArr[i] = " _ ";
+}
 //keep track of the wrong letters
+var wrong = [];
 
 //keep track of lives
-
+//how many guesses are left
 //keep track of score
 
-//keep track of guesses left
+//if lives = 0 game over
+if (stats.lives === 0) {
+    alert("Game Over!");
+}
 
-//function for onkeyup triggering functions for right or wrong choice
 
-//function for adjusting score 
+
+document.onkeyup = function (event) {
+
+    var letter = event.key.toLowerCase();
+    //function for onkeyup triggering functions for right or wrong choice
+    if (letter === "") {
+        car.honk();
+        reWriteStats();
+    }
+    //function for adjusting score 
 
     //function for you win at score = 10
 
-//function for removing lives
+    //function for removing lives
 
     //pseudo function for game over at 0 lives
 
-function wrongChoice(){
-    if
+    function wrongChoice() {
+        if
     //show the onkeyup character in wrong answer section
     if
     //-1 in the guesses left section
@@ -31,17 +56,17 @@ function wrongChoice(){
     //cycle to random new word
     
 };
-//keep track of the right letters when they get pressed
-function  rightChoice(){
-    //if on keyup is a character in the hidden word, 
+    //keep track of the right letters when they get pressed
+    function rightChoice() {
+        //if on keyup is a character in the hidden word, 
         //reveal the letter
         //do not affect guesses left
 
-    //IF a right choice completes a word
+        //IF a right choice completes a word
         //+1 to the score
         //choose new random word
         //clear guessed letters
 
-    //IF score gets to 10
-    //YOU WIN
-} 
+        //IF score gets to 10
+        //YOU WIN
+    } 
